9f85973f57be3cc55f1a20790d5fe6b07287044f
AsherLecover <asherlec5@gmail.com>
2020-07-02 18:14:35 +0300

    imitail commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,10 +18,11 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MainNavComponent } from './main-nav/main-nav.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 
 
 @NgModule({
@@ -33,7 +34,6 @@ import { MatListModule } from '@angular/material/list';
     ButtonsComponent,
     PicSubMainPageComponent,
     PicSubjectMainPageComponent,
-    MainNavComponent,
   ],
   imports: [
     BrowserModule,
@@ -50,7 +50,8 @@ import { MatListModule } from '@angular/material/list';
     LayoutModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]

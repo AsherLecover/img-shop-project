e14d701a6eaed1a7e733b4ce2c9edc3c35eafed4
AsherLecover <asherlec5@gmail.com>
2020-11-25 19:41:20 +0200

    initail commit

--- src/app/components/management/management.component.ts
+++ src/app/components/management/management.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ManagementService } from '../../servises/management.service';
 import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { ImgModel } from 'src/app/models/img-model';
 
 
 @Component({
@@ -43,32 +44,43 @@ export class ManagementComponent implements OnInit {
   idOfImgToAddDeleteEdit: number;
   editImgForm: FormGroup;
   categorySelected: string = ''
-
-
-  // selectedIdSubject: number = 0;
+  subId: number;
 
   constructor(
     private managementService: ManagementService,
     private fb: FormBuilder,
   ) { }
 
-
-
   ngOnInit(): void {
 
+    this.managementService.imgData$.subscribe( (data) => {
+      this.imgasListFromServer = data;
+})
+
+
+
+    this.editImgForm = this.fb.group({
+      photographer: ['', [Validators.required]],
+      imgDes: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      imgLongDes: ['', [Validators.required]],
+      imgUrl: ['', [Validators.required]]
+    });
   }
 
+
   selectSubject(event: any) {
-    this.managementService
-      .getSubjectImgesById(event.target.value)
+
+    this.managementService.getSubjectImgesById(event.target.value)
       .subscribe((data: imgModel[]) => {
+        
         this.imgasListFromServer = data;
-        this.categorySelected = data[0].imagesSubject;
+        this.managementService.imgData$.next(this.imgasListFromServer)
         this.idOfImgToAddDeleteEdit = event.target.value;
-
-
-        // this.addOption(event.target.value, data[0].imagesSubject)
-
+        this.subId = event.target.value;
+        this.imgSelected = data.find((img) => img.subId ==  this.idOfImgToAddDeleteEdit);
+        this.categorySelected = data[0].imagesSubject;
+      
       });
   }
 
@@ -80,71 +92,104 @@ export class ManagementComponent implements OnInit {
     this.massage = '? האם אתה בטוח שברצונך רוצה למחוק תמונה זו '
     this.deleteOrEditBtnName = 'אשר'
     this.idOfImgToAddDeleteEdit = id
+
+    
   }
 
   deleteImgFromServer(id) {
+
     id = this.idOfImgToAddDeleteEdit;
-    this.managementService.deleteImg(id).subscribe(data => {
+    console.log(this.subId);
+    
+    this.managementService.deleteImg(id, this.subId).subscribe( (data:any) => {
+
+      this.managementService.imgData$.next(data)
+
     })
   }
 
   editImgFromServer(id, imgDetailsToUpdate) {
     id = this.idOfImgToAddDeleteEdit;
-    this.managementService.editImgFromServer(id, imgDetailsToUpdate).subscribe((data) => {
+    this.managementService.editImgFromServer(id, imgDetailsToUpdate, this.subId).subscribe((data:any) => {
+      this.managementService.imgData$.next(data)
+      console.log('ggggggggggggggg',data);
+
 
     })
   }
 
-  addOption(id?: number) {
+  addOption() {
+    // subId = this.idOfImgToAddDeleteEdit;
     this.massage = ` הוספת תמונה בקטגוריית ${this.categorySelected}`
-    console.log('idddddddddddddddd', this.idOfImgToAddDeleteEdit);
+    this.subId = this.imgSelected.subId;
     this.alertBox = true
     this.addMode = true;
     this.deleteMode = false;
     this.editMode = false;
-    this.alertBox = true;
     this.deleteOrEditBtnName = ' הוספה לאתר'
-    this.imgSelected.photographer = ''
-    this.imgSelected.imgDes = ''
-    this.imgSelected.price = ''
-    this.imgSelected.imgLongDes = ''
-    this.imgSelected.imgUrl = ''
+
+
+    this.editImgForm = this.fb.group({
+      photographer: ['', [Validators.required]],
+      imgDes: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      imgLongDes: ['', [Validators.required,]],
+      imgUrl: ['', [Validators.required]]
+    });
   }
 
-  editOption(id) {
-    console.log('eeeedddiiittt option');
+  addImgToServer() {
+   let imgDataToAdd = this.editImgForm.value;
+   imgDataToAdd.imagesSubject = this.categorySelected;
+   imgDataToAdd.subId = this.subId;
+   imgDataToAdd.numOfItems = 1;
+   imgDataToAdd.img_id = this.imgasListFromServer.length;
+   
+    this.managementService.addImgToServer(imgDataToAdd).subscribe( (data:any) => {
+      this.managementService.imgData$.next(data)
 
-    this.imgSelected = this.imgasListFromServer.find((img) => img.id == id)
+      
+    })
+   }
+
+  editOption(id) {
+    this.imgSelected = this.imgasListFromServer.find((img) => img.id == id);
+
     this.idOfImgToAddDeleteEdit = this.imgSelected.id;
     this.imgUrl = this.imgSelected.imgUrl
     this.alertBox = true;
     this.editMode = true;
     this.deleteMode = false;
-    this.addMode = false
+    this.addMode = true;
     this.massage = 'ניתן לערוך כל אחד מן השדות הללו ';
     this.deleteOrEditBtnName = 'שמירת שינויים';
 
     this.editImgForm = this.fb.group({
-      photographer: [this.imgSelected.photographer, [Validators.required, Validators.minLength(2), Validators.pattern('^[0-9]+$')]],
+      photographer: [this.imgSelected.photographer, [Validators.required]],
       imgDes: [this.imgSelected.imgDes, [Validators.required]],
-      price: [this.imgSelected.price, [Validators.required, Validators.minLength(2)]],
-      imgLongDes: [this.imgSelected.imgLongDes, [Validators.required, Validators.pattern('^[ 0-9]*$'), Validators.minLength(17)]],
+      price: [this.imgSelected.price, [Validators.required]],
+      imgLongDes: [this.imgSelected.imgLongDes, [Validators.required]],
       imgUrl: [this.imgSelected.imgUrl, [Validators.required]]
     });
   }
 
   onClose() {
     this.alertBox = false;
-    if (this.deleteMode) {
+    if (this.deleteMode) {  
       this.deleteImgFromServer(this.idOfImgToAddDeleteEdit)
     }
     else if (this.editMode) {
-      console.log('img ddddddd', this.editImgForm.value);
-
-
       this.editImgFromServer(this.idOfImgToAddDeleteEdit, this.editImgForm.value)
-
+      console.log(222222); 
     }
+    else if (this.addMode) {
+      this.addImgToServer()
+      console.log(3333333);
+    }
+  }
+
+  onCloseBox(){
+    this.alertBox = false;
   }
 
   save(event) {
@@ -162,6 +207,6 @@ export interface imgModel {
   numOfItems: number
   photographer: string
   price: string
-  subId: string
+  subId: number
 
 }
--- src/app/servises/management.service.ts
+++ src/app/servises/management.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { Subject } from 'rxjs';
+import { imgModel } from '../components/management/management.component';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ManagementService {
- 
+  imgData$ = new Subject< any>();
+  
   constructor(private http: HttpClient) {}
 
   getSubjectImgesById(subId) {
     return this.http.get<any>(`${environment.apiUrl}/management/${subId}`);
   }
 
-  deleteImg(id:number) {
-    return this.http.delete(`${environment.apiUrl}/management/${id}`);
+  deleteImg(id: number,subId: number) {
+    return this.http.delete(`${environment.apiUrl}/management/${id}/${subId}`);
   }
 
-  editImgFromServer(id: any, imgDetailsToUpdate:{}) {
-    return this.http.patch(`${environment.apiUrl}/management/${id}`, {imgDetailsToUpdate})
+  editImgFromServer(id: any, imgDetailsToUpdate: {},subId) {
+    return this.http.patch(`${environment.apiUrl}/management/${id}/${subId}`, {imgDetailsToUpdate});
+  }
+  addImgToServer(imgDataToAdd) {
+    return this.http.post(`${environment.apiUrl}/management`,{imgDataToAdd})
   }
 }
